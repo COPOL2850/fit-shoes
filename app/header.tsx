7523213612f23e4bc4fd9b5c96dc982131020b9b
A.Toraby <copoltorabyfarsani@gmail.com>
2023-12-15 18:19:06 +0330

    header ; logo created

---
+++ app/header.tsx
@@ -0,0 +1,27 @@
+interface layout {
+  id: string;
+  src: string;
+}
+
+import Image from "next/image";
+export default async function Header() {
+  const layuotResponse = await fetch("http://localhost:4500/layout", {
+    cache: "no-store",
+  });
+  const layoutData: layout[] = await layuotResponse.json();
+  
+
+  return (
+    <div className="w-full h-12 bg-orange-700 ">
+      <div className="h-full w-48  flex justify-center items-center ">
+        <div className="w-10 h-full   flex justify-center items-center">
+          <Image  src={layoutData[0].src} width={30} height={30} alt={layoutData[0].id} />
+          
+        </div>
+        <div className=" h-full w-36 flex justify-center items-center">
+            <Image src={layoutData[1].src} width={144} height={40} alt={layoutData[1].id}/>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
+import Header from './header'
 
 export default function Home() {
   return (
     <div className=''>
+      <Header/>
+      
     
     </div>
       )
